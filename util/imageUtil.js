4951fbf226b81450327b000494ec4f7673f04ca6
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-08-10 09:53:54 +1000

    Fixed incorrect import

--- util/imageUtil.js
+++ util/imageUtil.js
@@ -1,4 +1,4 @@
-import { read } from "jimp";
+import { Jimp } from "jimp";
 
 async function validatePfp(b64) {
     if (typeof b64 !== 'string') return false;
@@ -8,7 +8,7 @@ async function validatePfp(b64) {
 
     const buffer = Buffer.from(b64, 'base64');
 
-    return await read(buffer).then(function (img) {
+    return await Jimp.read(buffer).then(function (img) {
         return img.bitmap.width > 0 && img.bitmap.height > 0 && img.bitmap.width <= process.env.MAX_PFP_SIZE && img.bitmap.height <= process.env.MAX_PFP_SIZE;
     }).catch(_ => false);
 }
