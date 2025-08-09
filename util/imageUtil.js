e8c71ec77e2ed704e9ff93a6307b253164ef9b15
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-08-10 09:52:03 +1000

    Created pfp validator helper function

---
+++ util/imageUtil.js
@@ -0,0 +1,16 @@
+import { read } from "jimp";
+
+async function validatePfp(b64) {
+    if (typeof b64 !== 'string') return false;
+
+    const regex = /^data:image\/(png|jpeg|jpg);base64,/;
+    if (!regex.test(b64)) return false;
+
+    const buffer = Buffer.from(b64, 'base64');
+
+    return await read(buffer).then(function (img) {
+        return img.bitmap.width > 0 && img.bitmap.height > 0 && img.bitmap.width <= process.env.MAX_PFP_SIZE && img.bitmap.height <= process.env.MAX_PFP_SIZE;
+    }).catch(_ => false);
+}
+
+export default validatePfp;
