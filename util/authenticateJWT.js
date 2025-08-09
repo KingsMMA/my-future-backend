bd2cdcf6a63b8d24cd172f55a1b6001af5e02701
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-08-09 20:18:41 +1000

    Setup auth methods

--- util/authenticateJWT.js
+++ util/authenticateJWT.js
@@ -16,5 +16,21 @@ function authLoggedIn(req, res, next) {
     });
 }
 
-module.exports = authLoggedIn;
+const authFactory = requiredRole => {
+    return function(req, res, next) {
+        authLoggedIn(req, res, () => {
+            if (req.user.accountType !== requiredRole) {
+                return res.status(403).json({ success: false, message: "You don't have the necessary permissions to access this endpoint" })
+            }
+
+            next();
+        });
+    }
+}
+
+const authCitizenOnly = authFactory("citizen");
+const authBusinessOnly = authFactory("business");
+const authGovernmentOnly = authFactory("government");
+
+module.exports = { authLoggedIn, authCitizenOnly, authBusinessOnly, authGovernmentOnly };
 
